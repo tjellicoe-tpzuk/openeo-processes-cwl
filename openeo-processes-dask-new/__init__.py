b56295954d3b8c4d603550900804592cf93b81bc
tjellicoe-tpzuk <149666043+tjellicoe-tpzuk@users.noreply.github.com>
2023-12-08 08:34:35 +0000

    Add files via upload

---
+++ openeo-processes-dask-new/__init__.py
@@ -0,0 +1,5 @@
+## removed above as this is now a local version with no other metadata available
+
+# import importlib.metadata
+
+# __version__ = importlib.metadata.version("openeo_processes_dask")
